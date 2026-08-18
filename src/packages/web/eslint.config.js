1fa333588de406c9572c09dca3394d3673f99e5f
Ilya Saunkin <eli@datamoil.com>
2026-08-18 11:41:02 +0100

    [web] Add eslint

---
+++ src/packages/web/eslint.config.js
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['src/**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+];
